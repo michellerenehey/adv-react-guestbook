3b129a34cf46876778b8b929ac0c41e5e23aa021
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-07 16:18:45 -0800

    added submitted username to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,5 +3,7 @@ import { useUser } from '../../context/UserContext';
 
 export default function Header() {
   const { user } = useUser();
-  return <div className="Header">hello there, {user}</div>;
+  return (
+    <div className="Header">{user ? <p>hello there, {user}</p> : <p>hello there, old pal</p>}</div>
+  );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,13 +1,32 @@
 import './Home.css';
 import { useUser } from '../../context/UserContext';
+import { useMessages } from '../../context/MessageContext';
+import { useState } from 'react';
 
 export default function Home() {
+  const [newMessage, setNewMessage] = useState('');
+  const [name, setName] = useState('');
+
   const { user, setUser } = useUser();
+  const { messages, setMessages } = useMessages();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser(name);
+    setMessages([...messages, { name, message: newMessage }]);
+    setNewMessage('');
+  };
 
   return (
     <div>
+      <h1>Guestbook!</h1>
       <label>Name</label>
-      <input value={user} onChange={(e) => setUser(e.target.value)} />
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <form onSubmit={handleSubmit}>
+        <label>Enter a message:</label>
+        <textarea value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
