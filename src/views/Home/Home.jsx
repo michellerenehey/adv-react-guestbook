dce3ac9b5cea41cc245245866c14f72a678bbce7
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-08 14:05:51 -0800

    removed name upon 'sign-in'

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -7,7 +7,7 @@ import Friends from '../../components/Friends/Friends';
 export default function Home() {
   const [newMessage, setNewMessage] = useState('');
   const [name, setName] = useState('');
-  const { setUser } = useUser();
+  const { user, setUser } = useUser();
   const { messages, setMessages } = useMessages();
   const { userArray, setUserArray } = useUserArray();
 
@@ -24,12 +24,14 @@ export default function Home() {
     <div className="Home">
       <h1>Will you sign my yearbook?</h1>
       <div className="guestbook">
-        <div className="guestbook-name">
-          <label>Enter your name:</label>
-          <input value={name} onChange={(e) => setName(e.target.value)} />
-        </div>
         <div className="guestbook-entry">
           <form onSubmit={handleSubmit}>
+            {!user && (
+              <div className="guestbook-name">
+                <label>Enter your name:</label>
+                <input value={name} onChange={(e) => setName(e.target.value)} />
+              </div>
+            )}
             <label>Enter a message:</label>
             <textarea value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
             <div className="submit-button">
