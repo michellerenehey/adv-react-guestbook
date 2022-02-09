3eed06f66c5ffa38b61a1bdbd7abaf0a3e81385b
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-09 12:55:59 -0800

    creating routes, separating friend list to seperate component

--- src/views/Layout/Header/Header.jsx
+++ src/views/Layout/Header/Header.jsx
@@ -1,12 +1,19 @@
 import './Header.css';
 import { useUser } from '../../../context/UserContext';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   const { user } = useUser();
 
   return (
     <div className="Header">
+      <Link to="/" className="header-link">
+        Home
+      </Link>
       {user ? <p>last signed by {user}</p> : <p>~A vErY cOoL yEaRbOoK~</p>}
+      <Link to="/friends" className="header-link">
+        Friends
+      </Link>
     </div>
   );
 }
