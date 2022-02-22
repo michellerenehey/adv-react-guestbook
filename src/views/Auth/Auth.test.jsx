20c3e230e566632404c821c18c5f1c4a157972f7
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-22 09:54:46 -0800

    expanded snapshot test working

--- src/views/Auth/Auth.test.jsx
+++ src/views/Auth/Auth.test.jsx
@@ -1,17 +1,18 @@
-import {render} from '@testing-library/react'; 
+import {render, screen, waitFor} from '@testing-library/react'; 
 import {MemoryRouter, Route, Switch} from 'react-router-dom'
 import { UserProvider } from '../../context/UserContext';
 import { EntriesProvider } from '../../context/EntriesContext';
 import { ThemeProvider } from '../../context/ThemeContext';
 import Auth from './Auth.jsx'; 
 import Home from '../Home/Home.jsx'; 
+import userEvent from '@testing-library/user-event'; 
 
 test('user can login', async () => {
     const {container} = render(
     <UserProvider>
       <EntriesProvider>
         <ThemeProvider>
-            <MemoryRouter>
+            <MemoryRouter initialEntries={["/login"]}>
                 <Switch>
                     <Route exact path="/login">
                         <Auth />
@@ -27,4 +28,16 @@ test('user can login', async () => {
     ); 
 
     expect(container).toMatchSnapshot();
-})
+
+    const userNameField = screen.getByPlaceholderText(/enter username/i); 
+    const passwordField = screen.getByPlaceholderText(/enter password/i); 
+    const submitButton = screen.getByRole('button', { name: /submit/i })
+
+    userEvent.type(userNameField, 'michellerenehey'); 
+    userEvent.type(passwordField, '123456'); 
+    userEvent.click(submitButton); 
+
+    return waitFor(() => {
+        screen.getByRole('heading', { name: /will you sign my yearbook\?/i })
+    })
+}); 
